00e066f01ef66112bca3ea4e726388f1b85662c2
让一切如梦 <luopengfei@luopengfeidemacbook-pro.local>
2016-11-17 10:43:57 +0800

    Initial Commit

---
+++ justPodsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  justPodsTest
+//
+//  Created by 骆鹏飞 on 2016/11/17.
+//  Copyright © 2016年 骆鹏飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
